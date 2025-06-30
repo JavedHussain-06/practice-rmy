7d8464cfa2f815c724d842350e98a7db368f781b
Javed Hussain <javedhussain23.20@gmail.com>
2025-06-30 18:43:32 +0530

    minor config change

--- RMY/vite.config.js
+++ RMY/vite.config.js
@@ -9,15 +9,12 @@ export default defineConfig({
     host: true,
     port: 5173,
     proxy: {
-      // Proxy for NPCI routes
       '/npci': {
         target: 'https://117.221.20.185',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/npci/, '/npci'),
       },
-
-      // Proxy for NHAI API routes
       '/api': {
         target: 'https://117.221.20.185',
         changeOrigin: true,
@@ -25,5 +22,6 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '/nhai/api'),
       },
     },
-  },
+  }
 });
+
